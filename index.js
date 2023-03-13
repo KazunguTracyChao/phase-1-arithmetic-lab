ed63b0d440f89a35622d3c030d663e0fc0b8dcfc
PhoebeTracyChaoKazungu <kazungutracy711@gmail.com>
2023-03-13 23:07:42 +0300

    Completed assignments

--- index.js
+++ index.js
@@ -1,2 +1,22 @@
 
 // Write your code here
+
+/*Multiply */
+let num1=62;
+let num2=1;
+
+let multiply;
+multiply = num1*num2;
+multiply=62;
+
+/* Random number */
+let random=Math.floor(Math.random()*1000+1)
+
+/*Mod*/
+
+let num3 =28;
+let num4 = 6;
+let mod = num3 % num4;
+
+/*Max value */
+let  max=Math.max(0,5,15,20,7);
